30320c3733fe5e24560384702d4307999813b271
Pawel <pawel_kantor@hotmail.com>
2020-04-29 23:09:26 +0200

    add: exercises

---
+++ app/src/main/java/pl/polsl/laboratorioweobecnosci/activities/admin/ExercisesActivity.kt
@@ -0,0 +1,18 @@
+package pl.polsl.laboratorioweobecnosci.activities.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import pl.polsl.laboratorioweobecnosci.R
+
+class ExercisesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercises)
+    }
+
+    fun onAddExerciseClick(view: View) {
+        /* TODO */
+    }
+}
