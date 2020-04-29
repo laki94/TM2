a6963a9affa2304a15f28de568ecd66be4181320
Pawel <pawel_kantor@hotmail.com>
2020-04-29 18:13:26 +0200

    add: main activity, default no action bar

---
+++ app/src/main/java/pl/polsl/laboratorioweobecnosci/activities/main/MainActivity.kt
@@ -0,0 +1,23 @@
+package pl.polsl.laboratorioweobecnosci.activities.main
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import pl.polsl.laboratorioweobecnosci.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onAdminPanelClick(view: View) {
+        /* TODO */
+    }
+
+    fun onStudentsPanelClick(view: View) {
+        /* TODO */
+    }
+}
