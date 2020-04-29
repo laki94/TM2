e7f03af2b259b534bf3c428fee4d673de641cf1d
Pawel <pawel_kantor@hotmail.com>
2020-04-29 19:28:49 +0200

    add: admin panel

---
+++ app/src/main/java/pl/polsl/laboratorioweobecnosci/activities/admin/AdminActivity.kt
@@ -0,0 +1,27 @@
+package pl.polsl.laboratorioweobecnosci.activities.admin
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import pl.polsl.laboratorioweobecnosci.R
+
+class AdminActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_panel)
+    }
+
+    fun onExercisesClick(view: View) {
+        /* TODO */
+    }
+
+    fun onRateClick(view: View) {
+        /* TODO */
+    }
+
+    fun onGenerateCSVClick(view: View) {
+        /* TODO */
+    }
+}
