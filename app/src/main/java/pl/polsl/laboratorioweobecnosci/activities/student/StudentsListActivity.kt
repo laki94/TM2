a232481fa15a2b75e30458c2c4618ee05e5fa7d0
Pawel <pawel_kantor@hotmail.com>
2020-04-29 23:00:05 +0200

    add: students list

---
+++ app/src/main/java/pl/polsl/laboratorioweobecnosci/activities/student/StudentsListActivity.kt
@@ -0,0 +1,18 @@
+package pl.polsl.laboratorioweobecnosci.activities.student
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import pl.polsl.laboratorioweobecnosci.R
+
+class StudentsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_students_list)
+    }
+
+    fun onAddStudentClick(view: View) {
+        /* TODO */
+    }
+}
